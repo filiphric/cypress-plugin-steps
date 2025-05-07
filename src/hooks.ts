d6bd5416db1666831e9bdb2270c592295e0fa08a
Kevin Korenhof <kevin.korenhof@incentro.com>
2025-05-07 12:04:46 +0200

    fix: place original err.message after steps to work better in CLI

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,17 +1,17 @@
 import { addStyles } from "./addStyles";
 
-Cypress.on('test:before:run', () => {
-
-  addStyles()
+Cypress.on("test:before:run", () => {
+  addStyles();
   window.logCalls = 1;
   window.testFlow = [];
+});
 
-})
-
-Cypress.on('fail', (err) => {
-  console.log(err)
+Cypress.on("fail", (err) => {
+  console.log(err);
   if (window.testFlow.length) {
-    err.message += `${'\n\n' + 'Test steps were:\n\n'}${window.testFlow.join('\n')}`;
+    err.message = `\n\n${"Test steps were:\n\n"}${window.testFlow.join(
+      "\n"
+    )}\n\n${err.message}`;
   }
   throw err;
 });
